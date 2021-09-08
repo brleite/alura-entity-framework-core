4c5bd60205e35d33a169eeaabc1efa92843ef043
Bruno Mayerle Leite <brleite@gmail.com>
2021-09-08 14:20:13 -0300

    Novas aulas

---
+++ Parte 2/Alura.Filmes.Aula7/Alura.Filmes/Alura.Filmes.App/Program.cs
@@ -0,0 +1,30 @@
+﻿using Alura.Filmes.App.Dados;
+using Alura.Filmes.App.Extensions;
+using Alura.Filmes.App.Negocio;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace Alura.Filmes.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var contexto = new AluraFilmesContexto())
+            {
+                contexto.LogSQLToConsole();
+
+                var ator1 = new Ator { PrimeiroNome = "Emma", UltimoNome = "Watson" };
+                var ator2 = new Ator { PrimeiroNome = "Emma", UltimoNome = "Watson" };
+                contexto.Atores.AddRange(ator1, ator2);
+                contexto.SaveChanges();
+
+                var emmaWatson = contexto.Atores
+                    .Where(a => a.PrimeiroNome == "Emma" && a.UltimoNome == "Watson");
+                Console.WriteLine($"Total de atores enconrados: {emmaWatson.Count()}.");
+
+            }
+        }
+    }
+}
---
+++ Parte 2/Alura.Filmes.Aula8/Alura.Filmes/Alura.Filmes.App/Program.cs
@@ -0,0 +1,33 @@
+﻿using Alura.Filmes.App.Dados;
+using Alura.Filmes.App.Extensions;
+using Alura.Filmes.App.Negocio;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace Alura.Filmes.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var contexto = new AluraFilmesContexto())
+            {
+                contexto.LogSQLToConsole();
+
+                var filme = new Filme();
+                filme.Titulo = "Senhor dos Anéis";
+                filme.Duracao = 120;
+                filme.AnoLancamento = "2000";
+                filme.Classificacao = "Qualquer";
+                filme.IdiomaFalado = contexto.Idiomas.First();
+                contexto.Entry(filme).Property("last_update").CurrentValue = DateTime.Now;
+
+                contexto.Filmes.Add(filme);
+                contexto.SaveChanges();
+
+
+            }
+        }
+    }
+}
