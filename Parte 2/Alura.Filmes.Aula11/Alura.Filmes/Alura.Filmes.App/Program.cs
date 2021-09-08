2e715b468a396bc255a51772a314ec10f810145d
Bruno Mayerle Leite <brleite@gmail.com>
2021-09-08 15:56:54 -0300

    Nova aula

---
+++ Parte 2/Alura.Filmes.Aula11/Alura.Filmes/Alura.Filmes.App/Program.cs
@@ -0,0 +1,31 @@
+﻿using Alura.Filmes.App.Dados;
+using Alura.Filmes.App.Extensions;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace Alura.Filmes.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var contexto = new AluraFilmesContexto())
+            {
+                contexto.LogSQLToConsole();
+
+                var sql = @"select a.* from actor a
+                  inner join top5_most_starred_actors filmes on filmes.actor_id = a.actor_id";
+
+                var atoresMaisAtuantes = contexto.Atores
+                    .FromSql(sql)
+                    .Include(a => a.Filmografia);
+
+                foreach (var ator in atoresMaisAtuantes)
+                {
+                    System.Console.WriteLine($"O ator {ator.PrimeiroNome} {ator.UltimoNome} atuou em {ator.Filmografia.Count} filmes.");
+                }
+
+            }
+        }
+    }
+}
