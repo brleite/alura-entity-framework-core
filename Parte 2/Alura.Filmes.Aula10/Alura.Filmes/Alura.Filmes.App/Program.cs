295fa55f8e3e12d6a580544b8a54785f015f340a
Bruno Mayerle Leite <brleite@gmail.com>
2021-09-08 15:02:52 -0300

    Novas aulas

---
+++ Parte 2/Alura.Filmes.Aula10/Alura.Filmes/Alura.Filmes.App/Program.cs
@@ -0,0 +1,33 @@
+﻿using Alura.Filmes.App.Dados;
+using Alura.Filmes.App.Extensions;
+using Alura.Filmes.App.Negocio;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace Alura.Filmes.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var contexto = new AluraFilmesContexto())
+            {
+                contexto.LogSQLToConsole();
+
+                Console.WriteLine("Clientes:");
+                foreach (var cliente in contexto.Clientes)
+                {
+                    Console.WriteLine(cliente);
+                }
+
+                Console.WriteLine("\nFuncionários:");
+                foreach (var func in contexto.Funcionarios)
+                {
+                    Console.WriteLine(func);
+                }
+
+            }
+        }
+    }
+}
---
+++ Parte 2/Alura.Filmes.Aula9/Alura.Filmes/Alura.Filmes.App/Program.cs
@@ -0,0 +1,37 @@
+﻿using Alura.Filmes.App.Dados;
+using Alura.Filmes.App.Extensions;
+using Alura.Filmes.App.Negocio;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace Alura.Filmes.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var contexto = new AluraFilmesContexto())
+            {
+                contexto.LogSQLToConsole();
+
+                var filme = new Filme();
+                filme.Titulo = "Cassino Royale";
+                filme.Duracao = 120;
+                filme.AnoLancamento = "2000";
+                filme.Classificacao = ClassificacaoIndicativa.MaioresQue14;
+                filme.IdiomaFalado = contexto.Idiomas.First();
+                contexto.Entry(filme).Property("last_update").CurrentValue = DateTime.Now;
+
+                contexto.Filmes.Add(filme);
+                contexto.SaveChanges();
+
+                var filmeInserido = contexto.Filmes.First(f => f.Titulo == "Cassino Royale");
+                Console.WriteLine(filmeInserido);
+                Console.WriteLine(filmeInserido.TextoClassificacao);
+
+
+            }
+        }
+    }
+}
